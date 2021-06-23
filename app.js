96dce1e991fabf9d9bc7c2037beac6f5d52d726d
Fdelo <federicodeloredo@gmail.com>
2021-06-23 20:33:08 -0300

    error link arreglado

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const path = require ("path");
 const app = express ();
 const publicPath = path.resolve(__dirname, './public');  
 app.use(express.static(publicPath));
-
+const publicPath2 = path.resolve(__dirname, './views');  
+app.use(express.static(publicPath2));
 
 app.listen(3005, () => {
   console.log ("Servidor corriendo en el puerto 3005");
