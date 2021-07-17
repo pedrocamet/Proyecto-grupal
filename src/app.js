008bd0c52bf9efab53e8e71fd0d5c244b411724c
Fdelo <federicodeloredo@gmail.com>
2021-07-16 23:34:39 -0300

    nada

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ app.set ("view engine", "ejs");
 const autosRoutes = require("./routes/autosRoutes");
 
 app.use("/", autosRoutes);
-
+app.use(express.static('./public'));
 const publicPath = path.resolve(__dirname, './public');  
 app.use(express.static(publicPath));
 const publicPath2 = path.resolve(__dirname, './views');  
@@ -15,5 +15,5 @@ app.use(express.static(publicPath2));
 app.listen(process.env.PORT || 3005, () => {
   console.log ("Servidor corriendo en el puerto 3005");
   });
-
+ 
   
