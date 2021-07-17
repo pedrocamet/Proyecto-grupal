afe56bd92c3784d5744398e302d843ad7a0ee964
Tomas Reynolds <tomasreynolds@ymail.com>
2021-07-17 12:10:01 -0300

    solucionando css y ruta

--- src/app.js
+++ src/app.js
@@ -7,13 +7,13 @@ const autosRoutes = require("./routes/autosRoutes");
 
 app.use("/", autosRoutes);
 
-const publicPath = path.resolve(__dirname, './public');  
+
+const publicPath = path.resolve(__dirname, '../public');  
 app.use(express.static(publicPath));
+
 const publicPath2 = path.resolve(__dirname, './views');  
 app.use(express.static(publicPath2));
 
 app.listen(process.env.PORT || 3005, () => {
   console.log ("Servidor corriendo en el puerto 3005");
   });
-
-  
