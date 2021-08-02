03620df776dbf0a5dc980bf0cc79f4235d7e8fa2
Fdelo <federicodeloredo@gmail.com>
2021-08-02 18:04:22 -0300

    creacion producto

--- src/controllers/autosController.js
+++ src/controllers/autosController.js
@@ -1,3 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+const multer = require ('multer');
+const { DefaultDeserializer } = require('v8');
+
+const productosFilePath = path.join(__dirname, '../data/productos.json');
+const productos = JSON.parse(fs.readFileSync(productosFilePath, 'utf-8'));
+
+
 const controlador =
 {
     home: (req, res) => {
@@ -19,11 +28,39 @@ const controlador =
     creacionProducto: (req, res) => {
         res.render ("./products/creacion-producto.ejs");
     },  
+    store: (req, res) => {
+    res.render ("./products/creacion-producto.ejs");
+        store: (req, res) => {
+            idNuevo = 0;
+            for (let s of productos){
+                if(idNuevo<s.id){
+                    idNuevo = s.id;
+            }
+        }
+            idNuevo++
+    
+            let nombreImagen =req.file.filename;
+            let productoNuevo = {
+                idAuto: idNuevo,
+                marca: req.body.marca,
+                model: req.body.modelo,
+                categoria: req.body.claseDeVehiculo,
+                ano:req.body.ano,
+                price:req.body.precioPorDia,
+                //description:  req.body.description,
+                image: nombreImagen
+            }
+    
+                productos.push(productoNuevo);
+             fs.writeFileSync(productosFilePath, JSON.stringify(productos, null, " "))
+            res.redirect('/');
+
+    }},
 
     producto: (req, res) => {
         res.render ("producto");
-    },
-
+    }
+    
 }
 
 
--- src/routes/autosRoutes.js
+++ src/routes/autosRoutes.js
@@ -7,8 +7,27 @@ router.get("/", autosController.home);
 router.get("/login", autosController.login);
 router.get("/carrito", autosController.carrito);
 router.get("/registro", autosController.registro);
+
+
+
+/***********lISTADO DE PRODUCTOS  ************/
 router.get("/producto", autosController.producto); 
+
+/***********CREATE ONE PRODUCT  ************/
 router.get("/creacion-producto", autosController.creacionProducto); 
+router.post("/creacion-producto", autosController.store); 
+
+
+/***********ONE PRODUCT DETAIL ************/
+//router.get("/productDetail/:id", autosController.productDetail); // detalle de un producto falta todo PARAMETRIZADO
+
+
+/***********EDIT A PRODUCT    ************/
+//router.get("/productDetail/:id/edit", autosController.editProduct); //formulario de edicion de productos 
+//router.put("/productDetail/:id/edit", autosController.editProduct);// edicion y almacenamiento de producto 
+
+/***********DELETE ONE PRODUCT  ************/
+//router.delete("/productDetail/:id/edit", autosController.editProduct);// eliminacion producto
 
 
 module.exports = router;
