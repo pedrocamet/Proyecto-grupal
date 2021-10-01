4104267de2cd5caea3a7567b992964b84e1777cb
Fdelo <federicodeloredo@gmail.com>
2021-10-01 15:20:20 -0300

    inicio carrito sobre detalle producto

---
+++ public/js/pruebaCarrito.js
@@ -0,0 +1,31 @@
+
+
+window.onload = function (){
+
+var botonAlqAhora = document.getElementById("boton-alquilar-ahora-T"); 
+
+var prodCompMarca= document.querySelector(".detalle-producto-marca-T").innerHTML
+var prodCompModelo= document.querySelector(".detalle-producto-modelo-T").innerHTML
+var prodCompPrecio = document.querySelector(".detalle-producto-precio-T").innerHTML
+
+let listaNombreProds = [];
+let prodComprado = [];
+
+
+console.log(prodComprado)
+botonAlqAhora.addEventListener("click", function(){
+ 
+  
+if (prodComprado.length == 0) {
+  prodComprado.push(prodCompMarca);
+  prodComprado.push(prodCompModelo);
+  prodComprado.push(prodCompPrecio);
+  listaNombreProds.push(prodComprado)
+
+}else{
+ 
+  alert("Usted no puede alquilar más de un Auto por operación")
+}
+  })
+
+}
