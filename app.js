847600a59978d2f5c98cf3ce7f893a147f783c50
Tomas Reynolds <tomasreynolds@ymail.com>
2021-10-25 00:28:49 -0300

    cors instalado para conectar apis a react

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const app = express ();
 app.set ("view engine", "ejs");
 var methodOverride = require('method-override');
 app.use(methodOverride("_method"));
+const cors = require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
+
 
 
 //Para captar la información que llega del formulario a través de POST
