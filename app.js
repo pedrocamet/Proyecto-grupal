400543619daa87d41c7a403d497080b05b43c53b
Fdelo <federicodeloredo@gmail.com>
2021-10-26 19:35:02 -0300

    api de ultimo cliente

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 const corsOptions ={
    origin:'*', 
    credentials:true,            
-   optionSuccessStatus:200,
+   optionSuccessStatus:200,   
 }
 
 app.use(cors(corsOptions))
