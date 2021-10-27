caa343aa3f21191942ba2e26e4b3a89318be8a5d
Fdelo <federicodeloredo@gmail.com>
2021-10-27 17:45:18 -0300

    paso del booton alguilar

--- public/js/pruebaCarrito.js
+++ public/js/pruebaCarrito.js
@@ -31,7 +31,9 @@ if (prodComprado.length == 0) {
  
   alert("Usted no puede alquilar más de un Auto por operación")
 }
+window.location.replace("../carrito")
   })
 console.log("ENTRO Y LLEGO ACA ? ")
 console.log(prodComprado)
+
 }
